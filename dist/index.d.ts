e0315cf8015aadb016ff439c9dbd4718d7f66b27
Marius Linders <marius.linders@orange-juice.nl>
2020-03-20 08:50:10 +0100

    rebuild, publish npm

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,10 @@
-declare type PromiseFn = (done: (value: any, error?: any) => void) => void | Promise<void>;
-export declare const setConcurrent: (n: number) => number;
-export declare const add: <R>(id: string, fn: PromiseFn) => Promise<R>;
+declare type PromiseFn<T> = (done: (value: T | null, error?: any) => void) => void | Promise<void>;
+export interface IQueueInstance {
+    add<T>(id: string | number, fn: PromiseFn<T>): Promise<T>;
+    nextId: () => number;
+    pause: () => void;
+    resume: () => void;
+    cancel: (id: string | number) => void;
+}
+export declare const createQueue: (concurrent?: number) => IQueueInstance;
 export {};
--- dist/index.js
+++ dist/index.js
@@ -1,52 +1,77 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var queue = {};
-var order = [];
-var concurrent = 2;
-exports.setConcurrent = function (n) {
-    return concurrent = n;
-};
-exports.add = function (id, fn) {
-    if (!queue[id]) {
-        order.push(id);
-        var resolver_1;
-        var rejector_1;
-        var promise = new Promise(function (res, rej) {
-            resolver_1 = res;
-            rejector_1 = rej;
+exports.createQueue = function (concurrent) {
+    if (concurrent === void 0) { concurrent = 2; }
+    var queue = {};
+    var order = [];
+    var id = 0;
+    var paused = false;
+    var add = function (id, fn) {
+        if (!queue[id]) {
+            order.push(id);
+            var resolver_1;
+            var rejector_1;
+            var promise = new Promise(function (res, rej) {
+                resolver_1 = res;
+                rejector_1 = rej;
+            });
+            queue[id] = {
+                promise: promise,
+                resolver: resolver_1,
+                rejector: rejector_1,
+                fn: fn,
+                busy: null,
+            };
+            if (allowNext())
+                run(order.shift());
+        }
+        return queue[id].promise;
+    };
+    var cancel = function (id) {
+        var i = order.indexOf(id);
+        if (id !== -1)
+            order.splice(i);
+    };
+    var nextId = function () {
+        return id++;
+    };
+    var pause = function () {
+        return paused = true;
+    };
+    var resume = function () {
+        paused = false;
+        while (allowNext() && order[0])
+            run(order.shift());
+    };
+    var run = function (id) {
+        if (!queue[id])
+            throw new Error("No queued task with id \"" + id + "\"");
+        queue[id].busy = true;
+        queue[id].fn(function (val, err) {
+            if (err)
+                queue[id].rejector(err);
+            else
+                queue[id].resolver(val);
+            queue[id].busy = false;
+            while (allowNext() && order[0])
+                run(order.shift());
+            delete queue[id];
         });
-        queue[id] = {
-            promise: promise,
-            resolver: resolver_1,
-            rejector: rejector_1,
-            fn: fn,
-            busy: null,
-        };
-        if (allowNext())
-            run(order.shift());
-    }
-    return queue[id].promise;
-};
-var run = function (id) {
-    if (!queue[id])
-        throw new Error("No queued task with id \"" + id + "\"");
-    queue[id].busy = true;
-    queue[id].fn(function (val, err) {
-        if (err)
-            queue[id].rejector(err);
-        else
-            queue[id].resolver(val);
-        queue[id].busy = false;
-        if (allowNext() && order[0])
-            run(order.shift());
-        delete queue[id];
-    });
-};
-var getRunning = function () {
-    return Object.keys(queue)
-        .map(function (x) { return queue[x]; })
-        .filter(function (x) { return x.busy === true; });
-};
-var allowNext = function () {
-    return getRunning().length < concurrent;
+    };
+    var allowNext = function () {
+        return !paused
+            && getRunning().length < concurrent;
+    };
+    var getRunning = function () {
+        return Object.keys(queue)
+            .map(function (x) { return queue[x]; })
+            .filter(function (x) { return x.busy === true; });
+    };
+    return {
+        add: add,
+        nextId: nextId,
+        pause: pause,
+        resume: resume,
+        cancel: cancel
+    };
 };
